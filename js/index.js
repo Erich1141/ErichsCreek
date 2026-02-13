2e9cd7373b21a3030607daa73f0af0e4e7a0e24a
Erich Rick <Erich1141@gmail.com>
2026-02-12 18:58:06 -0800

    added a fetch test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-//memoC:\Users\Erich\Documents\ErichLytleCreek
+//screen 1280,551
+var test=document.querySelector("#first")
+test.addEventListener("click", async() => {
+
+var weather= await fetch("https://erichs-real-server.onrender.com/test")
+
+var result=await weather.json()
+console.log(result)
+
+
+})
+

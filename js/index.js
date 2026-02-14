f62b57acddc85f000fee79214b5202bc2bae585f
Erich Rick <Erich1141@gmail.com>
2026-02-14 10:06:00 -0800

    created a new layout

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
 //screen 1280,551
-var test=document.querySelector("#first")
-test.addEventListener("click", async() => {
+var latest = document.querySelector("#latest");
+latest.addEventListener("click", async () => {
+  var product = document.querySelector("#product");
+  var office = document.querySelector("#forecastoffice");
+  var payload = { product: product.value.trim().toUpperCase(), office: office.value.trim().toUpperCase() };
 
-var weather= await fetch("https://erichs-real-server.onrender.com/test")
+  var weather = await fetch(
+    "https://erichs-real-server.onrender.com/API/weather/latest",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload)
+    }
+  );
 
-var result=await weather.json()
-console.log(result)
+
+if (!weather.ok) {
+    console.log("Server error:", weather.status, await weather.text());
+    return;
+  }
 
 
-})
-
+  var result = await weather.json();
+  console.dir(result);
+});

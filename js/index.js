6733f179998b2a04a1dca0f9365059592434c1fd
Erich Rick <Erich1141@gmail.com>
2026-03-06 20:34:53 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -118,10 +118,10 @@ temp.className="data"
 var fileGet=document.querySelector("#file")
 fileGet.addEventListener("click", async () => {
 
-var raw=await fetch("https://erichs-real-server.onrender.com/API/retrieve/testFile")
-console.log("sent file")
-console.log(raw.status)
-var response=await raw.text()
+var raw=await fetch("https://erichs-real-server.onrender.com/API/weather/latestObservation/MFLC1")
+
+
+var response=await raw.json()
 console.log(response)
 
 
